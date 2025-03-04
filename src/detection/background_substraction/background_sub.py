7012481d723ad834596c0da72c30b4460b5b5f18
Marin Boehm <marin.boehm@etu.univ-tours.fr>
2025-03-04 15:23:54 +0100

    ajout des fenetre d'occlusion pour background substraction

--- src/detection/background_substraction/background_sub.py
+++ src/detection/background_substraction/background_sub.py
@@ -9,18 +9,95 @@ from src.config.config_loader import load_config, get_video_path
 from src.detection.light.lowlight_test import enhance_image
 
 
-
+# Fonction pour dessiner les zones d'occlusion
 def draw_parallelogram(image, pts, color, thickness):
     pts = np.array(pts, np.int32)
     pts = pts.reshape((-1, 1, 2))
     cv2.polylines(image, [pts], isClosed=True, color=color, thickness=thickness)
 
 
+# Fonction pour définir les coordonnées des zones d'occlusion
+def define_coordinates_parallelograms(camera):
+    coord = []
+    match camera:
+        case 4:
+            x1, y1, x2, y2 = 730, 90, 1020, 100
+            x3, y3, x4, y4 = 1098, 120, 1250, 138
+            x5, y5, x6, y6 = 0, 0, 210, 0
+            x7, y7, x8, y8 = 400, 90, 690, 80
+            x9, y9, x10, y10 = 565, 150, 700, 150
+            x11, y11, x12, y12 = 380, 160, 460, 160
+            coord.append([(x1, y1), (x2, y2), (x2, y2 + 330), (x1, y1 + 245)])
+            coord.append([(x3, y3), (x4, y4), (x4, y4 + 390), (x3, y3 + 370)])
+            coord.append([(x5, y5), (x6, y6), (x6, y6 + 800), (x5, y5 + 800)])
+            coord.append([(x7, y7), (x8, y8), (x8, y8 + 70), (x7, y7 + 67)])
+            coord.append([(x9, y9), (x10, y10), (x10, y10 + 175), (x9, y9 + 135)])
+            coord.append([(x11, y11), (x12, y12), (x12, y12 + 65), (x11, y11 + 60)])
+        case 5:
+            x1, y1, x2, y2 = 795, 90, 1065, 100
+            x3, y3, x4, y4 = 1125, 120, 1275, 155
+            x5, y5, x6, y6 = 455, 75, 750, 75
+            x7, y7, x8, y8 = 0, 0, 210, 0
+            x9, y9, x10, y10 = 620, 130, 655, 130
+            x11, y11, x12, y12 = 425, 140, 515, 140
+            coord.append([(x1, y1), (x2, y2), (x2, y2 + 325), (x1, y1 + 230)])
+            coord.append([(x3, y3), (x4, y4), (x4, y4 + 378), (x3, y3 + 350)])
+            coord.append([(x5, y5), (x6, y6), (x6, y6 + 50), (x5, y5 + 60)])
+            coord.append([(x7, y7), (x8, y8), (x8, y8 + 800), (x7, y7 + 800)])
+            coord.append([(x9, y9), (x10, y10), (x10, y10 + 145), (x9, y9 + 135)])
+            coord.append([(x11, y11), (x12, y12), (x12, y12 + 70), (x11, y11 + 35)])
+        case 7:
+            x1, y1, x2, y2 = 770, 90, 1040, 100
+            x3, y3, x4, y4 = 1125, 120, 1265, 147
+            x5, y5, x6, y6 = 425, 75, 730, 75
+            x7, y7, x8, y8 = 0, 0, 210, 0
+            x9, y9, x10, y10 = 610, 130, 645, 130
+            x11, y11, x12, y12 = 415, 140, 500, 140
+            coord.append([(x1, y1), (x2, y2), (x2, y2 + 320), (x1, y1 + 230)])
+            coord.append([(x3, y3), (x4, y4), (x4, y4 + 378), (x3, y3 + 350)])
+            coord.append([(x5, y5), (x6, y6), (x6, y6 + 50), (x5, y5 + 60)])
+            coord.append([(x7, y7), (x8, y8), (x8, y8 + 800), (x7, y7 + 800)])
+            coord.append([(x9, y9), (x10, y10), (x10, y10 + 145), (x9, y9 + 135)])
+            coord.append([(x11, y11), (x12, y12), (x12, y12 + 70), (x11, y11 + 35)])
+        case 8:
+            x1, y1, x2, y2 = 800, 90, 900, 100
+            x3, y3, x4, y4 = 675, 95, 744, 100
+            x5, y5, x6, y6 = 1085, 140, 1279, 160
+            x7, y7, x8, y8 = 901, 100, 1070, 115 
+            x9, y9, x10, y10 = 0, 0, 210, 0
+            x11, y11, x12, y12 = 355, 90, 524, 80
+            coord.append([(x1, y1), (x2, y2), (x2, y2 + 190), (x1, y1 + 180)])
+            coord.append([(x3, y3), (x4, y4), (x4, y4 + 174), (x3, y3 + 184)])
+            coord.append([(x5, y5), (x6, y6), (x6, y6 + 360), (x5, y5 + 335)])
+            coord.append([(x7, y7), (x8, y8), (x8, y8 + 330), (x7, y7 + 295)])
+            coord.append([(x9, y9), (x10, y10), (x10, y10 + 800), (x9, y9 + 800)])
+            coord.append([(x11, y11), (x12, y12), (x12, y12 + 40), (x11, y11 + 40)])
+        case _:
+            print("Erreur : Camera non reconnue")
+            exit()
+    return coord
 
-def background_substraction(video_ref_dir, video_ref_name, video_test_dir, video_test_name, frame_tested):
+
+def background_substraction(camera, video_test_dir, video_test_name, frame_tested):
     # Charger la configuration
     config = load_config()
     video_path = get_video_path(config)
+
+    # nom du dossier de la vidéo de référence
+    video_ref_dir = "prise_0"
+
+    match camera:
+        case 4 :
+            video_ref_name = "vlc-record-2025-01-23-14h53m38s-rtsp___10.129.52.34_live_ID-STREAM-CAM4H-VID1-.mp4"
+        case 5 :
+            video_ref_name = "vlc-record-2025-01-23-14h53m36s-rtsp___10.129.52.34_live_ID-STREAM-CAM5H-VID1-.mp4"
+        case 7 :
+            video_ref_name = "vlc-record-2025-01-23-14h53m24s-rtsp___10.129.52.34_live_ID-STREAM-CAM7H-VID1-.mp4"
+        case 8 :
+            video_ref_name = "vlc-record-2025-01-23-14h53m33s-rtsp___10.129.52.34_live_ID-STREAM-CAM8H-VID1-.mp4"
+        case _ :
+            print("Erreur : Camera non reconnue")
+            exit()
 
     # Charger la vidéo de référence (sans objet)
     cap_ref = cv2.VideoCapture(os.path.join(video_path, video_ref_dir, video_ref_name))
@@ -39,37 +116,38 @@ def background_substraction(video_ref_dir, video_ref_name, video_test_dir, video
         exit()
 
     # Traitement luminosité
-    frame_ref_light = enhance_image(frame_ref)
+    #frame_ref_light = enhance_image(frame_ref)
+    frame_ref_light = frame_ref
+    
 
     # Aller chercher la nᵉ frame de la vidéo avec objet
     cap_current.set(cv2.CAP_PROP_POS_FRAMES, frame_tested)
     ret_cur, frame_cur = cap_current.read()
     if not ret_cur:
-        print("Erreur : Impossible de lire la vidéo actuelle à la frame 200")
+        print("Erreur : Impossible de lire la vidéo actuelle à la frame " + str(frame_tested))
         cap_ref.release()
         cap_current.release()
         exit()
 
     # Traitement luminosité
-    frame_cur_light = enhance_image(frame_cur)
+    #frame_cur_light = enhance_image(frame_cur)
+    frame_cur_light = frame_cur
+
 
     # --- AJOUTER UN RECTANGLE BLEU POUR LES ZONES À EXCLURE ---
-    x1, y1, x2, y2 = 770, 90, 1040, 100
-    x3, y3, x4, y4 = 1125, 120, 1265, 147
-    x5, y5, x6, y6 = 425, 75, 730, 75
+
+    coord = define_coordinates_parallelograms(camera)
 
     # Définir les points des parallélogrammes
+    parallelograms = []
+    parallelos = []
 
-    #pour fonction occlusion
-    parallelogram1 = np.array([[x1, y1], [x2, y2], [x2 + 0, y2 + 295], [x1 + 0, y1 + 200]], np.int32)
-    parallelogram2 = np.array([[x3, y3], [x4, y4], [x4 + 0, y4 + 378], [x3 + 0, y3 + 350]], np.int32)
-    parallelogram3 = np.array([[x5, y5], [x6, y6], [x6 + 0, y6 + 100], [x5 + 0, y5 + 100]], np.int32)
-
-    #pour affichage graphique
-    parallelo1 = [(x1, y1), (x2, y2), (x2 + 0, y2 + 295), (x1 + 0, y1 + 200)] 
-    parallelo2 = [(x3, y3), (x4, y4), (x4 + 0, y4 + 378), (x3 + 0, y3 + 350)]
-    parallelo3 = [(x5, y5), (x6, y6), (x6 + 0, y6 + 50), (x5 + 0, y5 + 55)]
-
+    for points in coord:
+        parallelogram = np.array(points, np.int32)
+        parallelo = points
+        parallelograms.append(parallelogram)
+        parallelos.append(parallelo)
+       
 
     # --- EXCLURE LES ZONES DES VITRES ---
 
@@ -84,9 +162,8 @@ def background_substraction(video_ref_dir, video_ref_name, video_test_dir, video
     _, thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
 
     # Supprimer les zones des vitres (mettre à zéro les pixels dans cette région)
-    cv2.fillPoly(thresh, [parallelogram1], 0) 
-    cv2.fillPoly(thresh, [parallelogram2], 0) 
-    cv2.fillPoly(thresh, [parallelogram3], 0) 
+    for parallelogram in parallelograms:
+        cv2.fillPoly(thresh, [parallelogram], 0)
 
     #  Appliquer des opérations morphologiques pour réduire le bruit
     kernel = np.ones((5, 5), np.uint8)
@@ -107,10 +184,9 @@ def background_substraction(video_ref_dir, video_ref_name, video_test_dir, video
 
 
     # Dessiner un rectangle bleu sur l'image pour visualiser la zone d'exclusion
-    
-    draw_parallelogram(output, parallelo1, (255, 0, 0), 2)
-    draw_parallelogram(output, parallelo2, (255, 0, 0), 2)
-    draw_parallelogram(output, parallelo3, (255, 0, 0), 2)
+    for parallelo in parallelos:
+        draw_parallelogram(output, parallelo, (255, 0, 0), 2)
+
 
     #  Afficher le résultat
     cv2.imshow("Objets detectes", output)
@@ -122,5 +198,6 @@ def background_substraction(video_ref_dir, video_ref_name, video_test_dir, video
     cv2.destroyAllWindows()
 
 
+#test
 
-background_substraction("prise_0", "vlc-record-2025-01-23-14h53m24s-rtsp___10.129.52.34_live_ID-STREAM-CAM7H-VID1-.mp4", "prise_16", "vlc-record-2025-01-23-16h05m02s-rtsp___10.129.52.34_live_ID-STREAM-CAM7H-VID1-.mp4", 3600)
+background_substraction(7,"prise_13", "vlc-record-2025-01-23-15h34m49s-rtsp___10.129.52.34_live_ID-STREAM-CAM7H-VID1-.mp4", 1000)
--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
 from src.app import create_app
 from src.config.config_loader import load_config, get_video_path
 from src.detection.objet_detection import process_videos
