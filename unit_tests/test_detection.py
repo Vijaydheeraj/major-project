8c64f84316e29bbc778f975e0a5f0e17763e3e58
nathaaanbraud <nathan.braud24@gmail.com>
2025-03-22 15:21:26 +0100

    Update unit tests in test_detection.py to include additional mocks

--- unit_tests/test_detection.py
+++ unit_tests/test_detection.py
@@ -2,6 +2,8 @@ import os
 import sys
 import unittest
 from unittest.mock import patch, MagicMock
+
+import cv2
 import pandas as pd
 import numpy as np
 
@@ -9,13 +11,13 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"
 
 # Functions to unit_tests
 from src.detection.objet_detection import process_frame, process_video, process_videos
-from app import app  
 
+@patch('src.detection.background_substraction.background_sub.match_frame_reference', return_value=np.zeros((480, 640, 3), dtype=np.uint8))
 @patch('src.detection.objet_detection.detection_yolov11')
 @patch('src.detection.objet_detection.detection_yolov11_fine_tuning')
 @patch('src.detection.objet_detection.classification_fine_tuning')
 class TestDetection(unittest.TestCase):
-    def test_process_frame(self, mock_classification, mock_fine_tuning, mock_detection):
+    def test_process_frame(self, mock_classification, mock_fine_tuning, mock_detection, mock_match_frame):
         """
         Test the process_frame function.
 
@@ -30,20 +32,24 @@ class TestDetection(unittest.TestCase):
         # Create a mock frame as a NumPy array
         mock_frame = np.zeros((480, 640, 3), dtype=np.uint8)
 
-        mock_detection.return_value = pd.DataFrame({'xmin': [0], 'ymin': [0], 'xmax': [100], 'ymax': [100], 'name': ['object'], 'confidence': [0.9]})
-        mock_fine_tuning.return_value = pd.DataFrame({'xmin': [10], 'ymin': [10], 'xmax': [90], 'ymax': [90], 'name': ['object'], 'confidence': [0.95]})
+        mock_detection.return_value = pd.DataFrame(
+            {'xmin': [0], 'ymin': [0], 'xmax': [100], 'ymax': [100], 'name': ['object'], 'confidence': [0.9]})
+        mock_fine_tuning.return_value = pd.DataFrame(
+            {'xmin': [10], 'ymin': [10], 'xmax': [90], 'ymax': [90], 'name': ['object'], 'confidence': [0.95]})
         mock_classification.return_value = [MagicMock(class_name='empty', confidence=0.85)]
+        mock_match_frame.return_value = np.zeros((480, 640, 3), dtype=np.uint8)  # Image de test valide
 
-        detections_df, classification_df_finetuning, additional_data, another_data, final_data = process_frame(mock_frame)
+        detections_df, detections_df_finetuning, classification_df_finetuning, detections_df_subtraction, detections_df_edgedetection = process_frame(mock_frame, 4)
 
         mock_detection.assert_called_once_with(mock_frame)
         mock_fine_tuning.assert_called_once_with(mock_frame)
         mock_classification.assert_called_once_with(mock_frame)
 
         self.assertIsInstance(detections_df, pd.DataFrame)
+        self.assertIsInstance(detections_df_finetuning, pd.DataFrame)
         self.assertIsInstance(classification_df_finetuning, list)
-        self.assertEqual(len(classification_df_finetuning), 1)
-        self.assertEqual(classification_df_finetuning[0].class_name, 'empty')
+        self.assertIsInstance(detections_df_subtraction, pd.DataFrame)
+        self.assertIsInstance(detections_df_edgedetection, pd.DataFrame)
 
 
 @patch('cv2.VideoCapture')
@@ -52,48 +58,61 @@ class TestDetection(unittest.TestCase):
 @patch('cv2.imshow')
 @patch('cv2.waitKey')
 @patch('cv2.destroyAllWindows')
+@patch('cv2.moveWindow')
 @patch('src.detection.objet_detection.detection_yolov11')
 @patch('src.detection.objet_detection.detection_yolov11_fine_tuning')
 @patch('src.detection.objet_detection.classification_fine_tuning')
-@patch('src.detection.objet_detection.extract_camera_data')  # Moulaye, Mock pour la fonction extract_camera_data
-@patch('src.detection.objet_detection.draw_detections')       # pour la fonction draw_detections
-@patch('src.detection.objet_detection.draw_classification')   # fonction draw_classification
+@patch('src.detection.objet_detection.extract_camera_data')
+@patch('src.detection.objet_detection.draw_detections')
+@patch('src.detection.objet_detection.draw_classification')
+@patch('cv2.cvtColor')
+@patch('src.detection.objet_detection.background_subtraction')
+@patch('src.detection.objet_detection.background_subtraction_on_edges')
+@patch('src.detection.objet_detection.detection_windows')
 class TestVideoProcessing(unittest.TestCase):
-    def test_process_video(self, mock_draw_classification, mock_draw_detections, mock_extract_camera_data, mock_classification, mock_fine_tuning, mock_detection, mock_destroy, mock_wait, mock_imshow, mock_resize, mock_named, mock_capture):
+    def test_process_video(self, mock_detection_windows, mock_background_subtraction_on_edges, mock_background_subtraction,
+                           mock_cvtColor, mock_draw_classification, mock_draw_detections, mock_extract_camera_data,
+                           mock_classification, mock_fine_tuning, mock_detection, mock_move_window, mock_destroy,
+                           mock_wait, mock_imshow, mock_resize, mock_named, mock_capture):
         """
         Test the process_video function.
 
         This test verifies that the process_video function correctly processes a video
         and calls the expected detection and classification functions.
-
-        Args:
-            mock_classification (MagicMock): Mock for the classification function.
-            mock_fine_tuning (MagicMock): Mock for the fine-tuning detection function.
-            mock_detection (MagicMock): Mock for the detection function.
-            mock_destroy (MagicMock): Mock for the cv2.destroyAllWindows function.
-            mock_wait (MagicMock): Mock for the cv2.waitKey function.
-            mock_imshow (MagicMock): Mock for the cv2.imshow function.
-            mock_resize (MagicMock): Mock for the cv2.resizeWindow function.
-            mock_named (MagicMock): Mock for the cv2.namedWindow function.
-            mock_capture (MagicMock): Mock for the cv2.VideoCapture function.
         """
+        # Mock de la capture vidéo
         mock_cap = MagicMock()
         mock_cap.isOpened.return_value = True
-        mock_frame = np.zeros((480, 640, 3), dtype=np.uint8)
+        mock_frame = np.zeros((480, 640, 3), dtype=np.uint8)  # Frame vide avec 3 canaux (RGB)
         mock_cap.read.side_effect = [(True, mock_frame), (True, mock_frame), (False, None)]
         mock_capture.return_value = mock_cap
+        mock_extract_camera_data.return_value = (4, "2025-03-22 12:00:00")
 
-        mock_detection.return_value = pd.DataFrame({'xmin': [0], 'ymin': [0], 'xmax': [100], 'ymax': [100], 'name': ['object'], 'confidence': [0.9]})
-        mock_fine_tuning.return_value = pd.DataFrame({'xmin': [10], 'ymin': [10], 'xmax': [90], 'ymax': [90], 'name': ['object'], 'confidence': [0.95]})
+        # Mock des détections et classifications
+        mock_detection.return_value = pd.DataFrame(
+            {'xmin': [0], 'ymin': [0], 'xmax': [100], 'ymax': [100], 'name': ['object'], 'confidence': [0.9]})
+        mock_fine_tuning.return_value = pd.DataFrame(
+            {'xmin': [10], 'ymin': [10], 'xmax': [90], 'ymax': [90], 'name': ['object'], 'confidence': [0.95]})
         mock_classification.return_value = [MagicMock(class_name='empty', confidence=0.85)]
 
+        # Mock de la détection des fenêtres
+        mock_detection_windows.return_value = []  # Pas de fenêtres détectées dans ce test simplifié
+
+        # Mock de la conversion de la couleur
+        mock_cvtColor.return_value = np.zeros((640, 640, 3), dtype=np.uint8)
+
+        # Appel à la fonction à tester
         process_video('fake_video.mp4', nb_of_img_skip_between_2=0)
 
+        # Vérifications : on vérifie que certaines fonctions ont été appelées
         mock_capture.assert_called_once_with('fake_video.mp4')
         self.assertEqual(mock_cap.read.call_count, 3)
         mock_detection.assert_called()
         mock_fine_tuning.assert_called()
         mock_classification.assert_called()
+        mock_background_subtraction.assert_called()
+        mock_background_subtraction_on_edges.assert_called()
+        mock_detection_windows.assert_called()
         mock_destroy.assert_called_once()
 
 
